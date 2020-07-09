2c0aa6ab5de8e048d13d49dc5430494f04eda4b6
attackonryan <1458727018@qq.com>
2020-07-09 20:47:02 +0800

    UI

---
+++ PSMS.UI/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //验证用户是否登录
+        public static bool logined = false;
+        //登录后设置level,对应不同的用户身份
+        public static int level = 0;
+        //存放主窗口实例，方便调用
+        public static MainWindow currentObj;
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainWindow.currentObj = this;
+            //初次进入需要进行登录操作
+            login loginBox = new login();
+            loginBox.Show();
+            this.Hide();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            login loginBox = new login();
+            loginBox.Show();
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ PSMS.UI/login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// login.xaml 的交互逻辑
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //这里后端判断逻辑,登录是否成功,成功设置logined为true
+            MainWindow.logined = true;
+            this.Close();
+            MainWindow.currentObj.Show();
+        }
+    }
+}
