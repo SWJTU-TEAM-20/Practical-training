ab6fef387cb91a6c5f0844a9d370158d400c70ec
忠涵 陈 <18782812147@163.com>
2020-07-09 17:00:17 +0800

    添加了一个部门类

---
+++ PSMS.Model/Dept.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSMS.Model
+{
+    public class Dept
+    {
+        //部门编号
+        public int Deptno { get; set; }
+        //部门名称
+        public string Name { get; set; }
+    }
+}
--- PSMS.Model/Manager.cs
+++ PSMS.Model/Manager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PSMS.Model
 {
-    class Manager
+    public class Manager
     {   /// <summary>
         /// 管理员类
         /// </summary>
@@ -15,36 +15,36 @@ namespace PSMS.Model
         public string name { get; set; }
         //密码
         public string password { get; set; }
-        //增加员工
-        public void Add()
-        { }
-        //删除员工
-        public void Dele()
-        { }
-        //查询员工
-        public void Query_P()
-        { }
-        //调动员工
-        public void Trans()
-        { }
-        //工资查询
-        public void Query_S()
-        { }
-        //修改员工
-        public void Modify_P()
-        { }
-        //统计出勤情况
-        public void Statistic()
-        { }
-        //设置加班费率
-        public void Set_Rate()
-        { }
-        //工资汇总
-        public void Summary_S()
-        { }
-        //数据输入到（工资表）
-        public void Input_D()
-        { }
+        ////增加员工
+        //public void Add()
+        //{ }
+        ////删除员工
+        //public void Dele()
+        //{ }
+        ////查询员工
+        //public void Query_P()
+        //{ }
+        ////调动员工
+        //public void Trans()
+        //{ }
+        ////工资查询
+        //public void Query_S()
+        //{ }
+        ////修改员工
+        //public void Modify_P()
+        //{ }
+        ////统计出勤情况
+        //public void Statistic()
+        //{ }
+        ////设置加班费率
+        //public void Set_Rate()
+        //{ }
+        ////工资汇总
+        //public void Summary_S()
+        //{ }
+        ////数据输入到（工资表）
+        //public void Input_D()
+        //{ }
 
     }
 }
