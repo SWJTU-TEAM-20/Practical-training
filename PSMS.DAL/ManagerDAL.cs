f533cdffab02ce8346a0f144140c506952557e09
忠涵 陈 <18782812147@163.com>
2020-07-09 21:51:37 +0800

    添加了UI层和一个数据交换层的登录接口，移除了原有的UI层

--- PSMS.DAL/ManagerDAL.cs
+++ PSMS.DAL/ManagerDAL.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PSMS.Model;
 namespace PSMS.DAL
 {
+    interface DalInterface
+    {
+        Manager UserLogin(string User_Name, string Password);
+    }
     class ManagerDAL
     {
+
+
     }
 }
