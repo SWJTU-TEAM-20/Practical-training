59ff01eb17960df31e79102780a7ea59b1780fa4
忠涵 陈 <18782812147@163.com>
2020-07-09 15:24:04 +0800

    添加了类库

--- PSMS.Model/Emp.cs
+++ PSMS.Model/Emp.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PSMS.Model
 {
-    public class Class1
+    public class Emp
     {
+        //员工编号
+        public int Empno { get; set; }
+
     }
 }
