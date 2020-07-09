22a020c9da77aa5af5c7fbb63b6621421a1dbad2
可仪 朱 <2405880706@qq.com>
2020-07-09 16:34:37 +0800

    添加了Manager类

---
+++ PSMS.Model/Manager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSMS.Model
+{
+    class Manager
+    {   /// <summary>
+        /// 管理员类
+        /// </summary>
+
+        //管理员姓名、登录账号
+        public string name { get; set; }
+        //密码
+        public string password { get; set; }
+        //增加员工
+        public void Add()
+        { }
+        //删除员工
+        public void Dele()
+        { }
+        //查询员工
+        public void Query_P()
+        { }
+        //调动员工
+        public void Trans()
+        { }
+        //工资查询
+        public void Query_S()
+        { }
+        //修改员工
+        public void Modify_P()
+        { }
+        //统计出勤情况
+        public void Statistic()
+        { }
+        //设置加班费率
+        public void Set_Rate()
+        { }
+        //工资汇总
+        public void Summary_S()
+        { }
+        //数据输入到（工资表）
+        public void Input_D()
+        { }
+
+    }
+}
+
