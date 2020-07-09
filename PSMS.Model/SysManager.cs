37adf75aee00984e32e87de5b5ea5bac24bede87
忠涵 陈 <18782812147@163.com>
2020-07-09 16:54:41 +0800

    添加了系统管理员类

---
+++ PSMS.Model/SysManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSMS.Model
+{
+    public class SysManager
+    {
+        //用户名
+        public string User_Name { get; set; }
+        //密码
+        public string Password { get; set; }
+    }
+}
